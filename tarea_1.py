8e6cec4c1ad628d9c9a402f3e0d0afdc9bfe2770
johanbecerra <joh.becerram@duocuc.cl>
2024-05-29 10:44:06 -0400

    se realizo la estructura del menu

---
+++ tarea_1.py
@@ -0,0 +1,24 @@
+import os
+import time
+import msvcrt
+
+print("1. Inicio de sesión")
+print("2. Registrar usuario")
+print("3. Eleiminar usuario")
+print("4. Salir")
+while True:
+    opc=int(input("ingrese opcion: "))
+    if opc==1:
+        pass
+    elif opc==2:
+        pass
+    elif opc==3:
+        pass
+    else:
+        print("ADIOS!")
+
+
+usuario={
+    "nombre": nombre,
+    "password": password
+}
